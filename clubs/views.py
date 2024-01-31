3ab2babe34b7a97b1f5f4432c3313ff8e07850fe
magdaprzybylska <104300366+magdaprzybylska@users.noreply.github.com>
2024-01-31 15:13:26 +0100

    added pre-commit and ran it against all files (#4)
    * added pre-commit and ran it against all files
    
    * added bandit and isort hooks

--- clubs/views.py
+++ clubs/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'clubs/index.html')
+    return render(request, "clubs/index.html")
