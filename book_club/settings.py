22c1aa4148cda3148d55a45a60604bc58c60a27d
Magda Przybylska <magda.przybylska0811@gmail.com>
2024-02-08 15:20:52 +0100

    added docker-compose setup, created .env file and updated bandit

--- book_club/settings.py
+++ book_club/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import dotenv_values
+
+env_config = dotenv_values(".env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&13wq7s8ga(z_27ck-jb8%3-p65&rt6gw3fab1nzjo6$!aey_!"
+SECRET_KEY = env_config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env_config["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env_config["ALLOWED_HOSTS"]]
 
 
 # Application definition
